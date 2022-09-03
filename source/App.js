5f35bec627b0bd9dad080458f779d35d8d9a8bbf
Matrix <1337816495@qq.com>
2022-09-04 03:09:58 +0800

    Update App.js

--- source/App.js
+++ source/App.js
@@ -1,5 +1,21 @@
 import React from "react";
+import Logo from "./Logo";
 
-function App () {}
+function App () {
+
+    return (
+        <div>
+            <Logo width="80" height="80"/>
+            <h1>Bookshelf</h1>
+            <div>
+                <button>Login</button>
+            </div>
+            <div>
+                <button>Registry</button>
+            </div>
+        </div>
+    );
+
+}
 
 export default App;
